f3cd1b3bdb17522b3e08895364b1ea6bd44395d4
Garves <garves44@gmail.com>
2020-05-12 04:16:34 -0600

    added fight function to track attacks from player and enemy

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,9 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 110;
+// check to see if the value of the playerHealth variable is greater then 0
+if (playerHealth > 0) {
+    console.log("Your player is still alive!");
+}
 var playerAttack = 11;
 
 // This log will keep track of player Name/health/Attack
@@ -19,10 +23,24 @@ var fight = function() {
     enemyHealth = enemyHealth - playerAttack;
     // Log a resulting message to the console so we know that it worked.
     console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
     // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
     playerHealth = playerHealth - enemyAttack;
     // Log a resulting message to the console so we know that it worked.
     console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+    // check players's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 };
 
 fight();
