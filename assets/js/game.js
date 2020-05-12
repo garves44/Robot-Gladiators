3d35be28a6b4c9d728a51e9a52026c8e5ffc60c4
Garves <garves44@gmail.com>
2020-05-12 03:37:34 -0600

    added a few console.log functions for testing

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("this is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName)
+console.log("This logs a string, good for leaving yourself a message!");
+// this will do math
+console.log(10+10+10);
+// what is this?
+console.log("Our robot's name is" + playerName)
+// This creats a function named "fight"
+function fight() {
+    window.alert("The fight has begun!")
+}
+// fight();
